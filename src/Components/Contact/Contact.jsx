770964f97ee4cb0b3870828922e274faf6afe20b
Franco Varone <francovarone@MacBook-Pro-de-Franco.local>
2021-12-29 17:57:45 -0300

    Va queriendo

---
+++ src/Components/Contact/Contact.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import s from './Contact.module.css'
+// import nodemailer from 'nodemailer'
+
+// const mailer = nodemailer.createTransport({
+//     host: "smtp.gmail.com",
+//     port: 465,
+//     secure: true, // true for 465, false for other ports
+//     auth: {
+//       user: "SzpryngierWasserman@gmail.com",
+//       pass: "fotoinmobiliaria", // Luego ubicar en el .env !!!!
+//     },
+//   });
+
+export default function Contact () {
+    const [data, setData] = useState({
+        name:'',
+        email:'',
+        number:'',
+    })
+
+    const onChange = (e) => {
+        setData({
+            ...data,
+            [e.target.name] : e.target.value
+        })
+    }
+    const onSubmit = async () => {
+        
+    }
+
+    return(
+        <form className={s.form} onSubmit={onSubmit}>
+            <input className={s.input} placeholder="Nombre" name='name' value={data.name} onChange={e => onChange(e)} />
+            <input className={s.input} placeholder="Email" name="email" value={data.email} onChange={e => onChange(e)} />
+            <input className={s.input} placeholder="11 2345 6789" value={data.number} name="number" onChange={e => onChange(e)} />
+            <button type="sumbit">Enviar</button>            
+        </form>
+    )
+}
